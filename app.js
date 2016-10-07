8f613e2136d0d7d775409fe79936b846326ff452
Kuzhimiyl <raghu.kuzhimiyl@teradata.com>
2016-10-07 13:34:11 -0400

    rk'

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function NarrowItDownController(MenuSearchService) {
              list.menulist="";
          list.search = function () {
 var searchItem= list.choice;
-   console.log('entry1");
+   console.log('list.choice");
  getMatchedMenuItem(searchItem);
         
         }
